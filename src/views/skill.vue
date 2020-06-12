c7845f750176b4a459ec64772956f57c112fd9d4
mkdk09 <mkdk099@gmail.com>
2020-06-12 14:48:55 +0900

    カードを追加

--- src/views/skill.vue
+++ src/views/skill.vue
@@ -1,8 +1,34 @@
 <template>
     <div class="skill container" id="skill">
         <h1>SKILL</h1>
+        <div class="card-deck row">
+            <div v-for="(item, key) in profileItems" v-bind:key="key" class="col-xs-12 col-md-4">
+                <profile-item v-bind="item" />
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import ProfileItem from '../components/ProfileItem.vue'
+export default {
+    components: {
+        ProfileItem
+    },
+    data() {
+      return {
+        profileItems: [
+          { title: 'title1', description: 'description1', comment: 'comment1' },
+          { title: 'title2', description: 'description2', comment: 'comment2' },
+          { title: 'title3', description: 'description3', comment: 'comment3' },
+          { title: 'title4', description: 'description4', comment: 'comment4' },
+          { title: 'title5', description: 'description5', comment: 'comment5' },
+          { title: 'title6', description: 'description6', comment: 'comment6' }
+        ]
+      }
+    }
+}
+</script>
 
 <style scoped>
 .skill {
